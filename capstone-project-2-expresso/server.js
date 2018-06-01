a48bc947b3e225dd9e86f85efbcffb2766e84188
sepuckett86 <sepuckett86@gmail.com>
2018-06-01 13:01:10 +0200

    create table works

--- capstone-project-2-expresso/server.js
+++ capstone-project-2-expresso/server.js
@@ -5,10 +5,12 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-app.use(cors());
-app.use(morgan());
-app.use(bodyParser());
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser());
 
 const PORT = process.env.PORT || 4000;
 
