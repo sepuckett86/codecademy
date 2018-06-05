2ed83289ac321f282583282332e47a29f632cbe1
sepuckett86 <sepuckett86@gmail.com>
2018-06-05 15:55:46 +0200

    works on front-end

--- capstone-project-2-expresso/server/apiRouters/menus.js
+++ capstone-project-2-expresso/server/apiRouters/menus.js
@@ -2,7 +2,7 @@ const express = require('express');
 const menusRouter = express.Router();
 
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || '../../database.sqlite')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
 menusRouter.get('/', (req, res, next) => {
   const sql = 'SELECT * FROM Menu';
